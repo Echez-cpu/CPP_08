95e7e4a64fc65a17f9fc67333517cab9aa2aa165
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-24 14:09:00 +0100

    Create easyfind.hpp

---
+++ ex00/includes/easyfind.hpp
@@ -0,0 +1,19 @@
+#ifndef  EASYFIND_HPP
+#define  EASYFIND_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <stdexcept>
+
+
+class NoValueException : public std::exception {
+	public:
+		virtual const char *	what() const throw();
+};
+
+template<typename T>
+int	easyfind(T const & arr, int value);
+
+#endif
