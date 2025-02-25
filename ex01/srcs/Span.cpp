ff397f333fe7d1dfff7105ce7cd6546791ace06c
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-25 15:42:15 +0100

    Update Span.cpp

--- ex01/srcs/Span.cpp
+++ ex01/srcs/Span.cpp
@@ -25,3 +25,34 @@ void Span::addNumber(int n) {
 	this->input_array.push_back(n);
 	this->actual_size++;
 }
+
+
+void Span::bigSpan(int n) {
+    if (this->actual_size + n > this->input_maxSize) {
+        std::cout << "Can't add " << n << " elements to the array, max_size will be surpassed." << std::endl;
+        return;
+    }
+
+    int nb = 0;
+    for (int i = 0; i < n; i++) {
+        nb = rand() % 100000 + 1;  // Generate random number between 1-100000
+        this->addNumber(nb);  // Add number to Span
+
+        if (i % 1000 == 0) {
+            std::cout << "current size: " << this->actual_size << std::endl << "current randonly generated number " : " << nb << std::endl;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
