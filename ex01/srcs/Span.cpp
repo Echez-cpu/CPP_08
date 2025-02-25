9ca8b0d5c6c2fb647e439f228b1c484d99b639d9
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-25 20:41:20 +0100

    Update Span.cpp

--- ex01/srcs/Span.cpp
+++ ex01/srcs/Span.cpp
@@ -45,15 +45,6 @@ void Span::addNumber(int n) {
 }*/
 
 
-template <typename InputIterator>
-void Span::addRange(InputIterator begin, InputIterator end) {
-    if (std::distance(begin, end) > static_cast<int>(input_maxSize - input_array.size())) {
-        throw MaxNumbersException();
-    }
-    input_array.insert(input_array.end(), begin, end);
-}
-
-
 int 	Span::shortestSpan() const {
 	
 	if (input_array.size() <  2)
